cb411db3ddfef9706bfd574e789ef0ca346eb84f
Nele Hirsch <nele@ebildungslabor.de>
2019-04-01 14:14:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
+<!--
+	Story by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html>
 	<head>
-		<meta charset='utf-8'/>
-		<title>Presentation Feedback Tool</title>
+		<title>BitteFeedback - offenes Online-Feedback-Tool</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
-	<body style="margin: 0 auto 0 auto; width: 720px; margin-top: 100px;">
-		<?php 
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="divided">
+
+				<!-- One -->
+					<section class="banner style2 content-align-left fullscreen">
+						<div class="content">
+							
+<p><?php 
 			$included = true;
 			if (isset($_POST['title']) || isset($_GET['code']) || isset($_GET['secret'])) {
 				require('config.php');
@@ -24,10 +40,35 @@
 			else {
 				require('frontpage.php');
 			}
-		?>
-		<footer style="display: block; margin-top: 125px; margin-bottom: 75px; color: #555;">
-			This website is
-			<a style="color: #555;" href="https://github.com/lgommans/presentation-feedback">free software.</a>
-		</footer>
+		?></p>
+							
+						</div>
+						<div class="image">
+							<img src="images/back.jpg" alt="" />
+						</div>
+					</section>
+
+				
+
+				<!-- Footer -->
+					<footer class="wrapper style1 align-center">
+						<div class="inner">
+					
+<p>Mit BitteFeedback.de kannst Du einfach und unkompliziert Feedback geben oder erfragen. Es werden keine persönlichen Daten erhoben. Eine Registrierung ist nicht erforderlich. Alle Eintragungen werden nach 14 Tagen gelöscht. Nähere Informationen findest Du im <a href=/impressum.html>Impressum und den Hinweisen zum Datenschutz</a>		
+							<p><small>BitteFeedback.de beruht auf freier Software. Der Code stammt von <a href="http://lgms.nl/" target="blank">Luc Gommans</a>. Du findest ihn <a href="https://github.com/lgommans" target="blank">bei Github</a>. Diese Installation habe ich für meine persönliche Nutzung im <a href="https://ebildungslabor.de target="blank">eBildungslabor</a> online gestellt und dazu den Code leicht modifiziert, ins Deutsche übersetzt und mithilfe eines Templates von <a href="https://html5up.net">HTML5 UP</a> gestaltet. Du findest <a href=>hier die angepasste Version</a></small> - z.B. für eine eigene Installation.</p>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
 	</body>
 </html>
